f6a9dd5cbb708b18c0624f00070cbfd265b2b18f
leo41271 <33276762+leo41271@users.noreply.github.com>
2018-11-29 19:40:40 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -36,6 +36,10 @@ def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
+def Reply(text):
+	if text =="h1" || text =="gg":
+		return TextSendMessage(text = "hello")
+
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
