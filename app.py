382d9f5ffb020b0dde1a7764c89cdfded9f689ce
leo41271 <33276762+leo41271@users.noreply.github.com>
2018-12-06 19:01:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -47,25 +47,25 @@ def handle_message(event):
 
 def Button:
     message = TemplateSendMessage(
-           alt_text='Buttons template',
-           template=ButtonsTemplate(
-           thumbnail_image_url='https://example.com/image.jpg',
-           title='Menu',
-           text='Please select',
-           actions=[
-               PostbackTemplateAction(
-                   label='postback',
-                   text='postback text',
-                   data='action=buy&itemid=1'
-               ),
-               MessageTemplateAction(
-                   label='message',
-                   text='message text'
-               ),
-               URITemplateAction(
-                   label='uri',
-                   uri='http://example.com/'
-               )]))
+        alt_text='Buttons template',
+        template=ButtonsTemplate(
+        thumbnail_image_url='https://example.com/image.jpg',
+        title='Menu',
+        text='Please select',
+        actions=[
+            PostbackTemplateAction(
+                label='postback',
+                text='postback text',
+                data='action=buy&itemid=1'
+            ),
+            MessageTemplateAction(
+                label='message',
+                text='message text'
+            ),
+            URITemplateAction(
+                label='uri',
+                uri='http://example.com/'
+            )]))
 '''
 def KeyWord(text):
     KeyWordDict = {"140GF":"140GF4man",
