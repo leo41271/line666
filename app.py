10adcd020f4486ae24fc8191c47c7c8ccdd1247f
leo41271 <33276762+leo41271@users.noreply.github.com>
2018-12-20 18:33:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,39 +35,47 @@ def callback():
 #關鍵字系統
 
 #關鍵字系統
-def KeyWord(event):
-    KeyWordDict = {"你好":"你也好啊",
-                   "你是誰":"我是大帥哥",
-                   "帥":"帥炸了",
-                   "差不多了":"讚!!!"}
+def Keyword(event):
+    KeyWordDict = {"你好":["text","你也好啊"],
+                   "你是誰":["text","我是大帥哥"],
+                   "差不多了":["text","讚!!!"],
+                   "帥":["sticker",'1','120']}
 
     for k in KeyWordDict.keys():
         if event.message.text.find(k) != -1:
-            return [True,KeyWordDict[k]]
-    return [False]
+            if KeyWordDict[k][0] == "text":
+                line_bot_api.reply_message(event.reply_token,TextSendMessage(text = KeyWordDict[k][1]))
+            elif KeyWordDict[k][0] == "sticker":
+                line_bot_api.reply_message(event.reply_token,StickerSendMessage(
+                    package_id=KeyWordDict[k][1],
+                    sticker_id=KeyWordDict[k][2]))
+            return True
+    return False
 
 #按鈕版面系統
 def Button(event):
-    return TemplateSendMessage(
-        alt_text='特殊訊息，請進入手機查看',
-        template=ButtonsTemplate(
-            thumbnail_image_url='https://github.com/54bp6cl6/LineBotClass/blob/master/logo.jpg?raw=true',
-            title='HPClub - Line Bot 教學',
-            text='大家學會了ㄇ',
-            actions=[
-                PostbackTemplateAction(
-                    label='還沒',
-                    data='還沒'
-                ),
-                MessageTemplateAction(
-                    label='差不多了',
-                    text='差不多了'
-                ),
-                URITemplateAction(
-                    label='幫我們按個讚',
-                    uri='https://www.facebook.com/ShuHPclub'
-                )
-            ]
+    line_bot_api.reply_message(event.reply_token,
+        TemplateSendMessage(
+            alt_text='特殊訊息，請進入手機查看',
+            template=ButtonsTemplate(
+                thumbnail_image_url='https://github.com/54bp6cl6/LineBotClass/blob/master/logo.jpg?raw=true',
+                title='HPClub - Line Bot 教學',
+                text='大家學會了ㄇ',
+                actions=[
+                    PostbackTemplateAction(
+                        label='還沒',
+                        data='還沒'
+                    ),
+                    MessageTemplateAction(
+                        label='差不多了',
+                        text='差不多了'
+                    ),
+                    URITemplateAction(
+                        label='幫我們按個讚',
+                        uri='https://www.facebook.com/ShuHPclub'
+                    )
+                ]
+            )
         )
     )
 
@@ -83,13 +91,8 @@ def Command(event):
 #回覆函式，指令 > 關鍵字 > 按鈕
 def Reply(event):
     if not Command(event):
-        Ktemp = KeyWord(event)
-        if Ktemp[0]:
-            line_bot_api.reply_message(event.reply_token,
-                TextSendMessage(text = Ktemp[1]))
-        else:
-            line_bot_api.reply_message(event.reply_token,
-                Button(event))
+        if not Keyword(event):
+            Button(event)
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
@@ -111,14 +114,14 @@ def handle_postback(event):
     if command[0] == "還沒":
         line_bot_api.reply_message(event.reply_token, 
             TextSendMessage(text="還沒就趕快練習去~~~"))
-
+        
 @handler.add(MessageEvent, message=StickerMessage)
 def handle_sticker_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         StickerSendMessage(
-            package_id=event.message.package_id,
-            sticker_id=event.message.sticker_id)
+            package_id='1',
+            sticker_id='410')
     )
 import os
 if __name__ == "__main__":
