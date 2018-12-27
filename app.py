e6dbaa0f96dccd99fa4236eb2bc1544e1340e486
leo41271 <33276762+leo41271@users.noreply.github.com>
2018-12-27 18:42:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,71 +36,82 @@ def callback():
 #Sublime
 #_______________________________________________________________________
 
-#關鍵字系統
+#設定帳號class
+class user:
+    def __init__(self,ID,Name,Situation):
+        self.Name = Name
+        self.ID = ID
+        self.Situation = Situation
 
-#會員系統
-def GetUserlist():
-    userlist = {}
-    file = open('users','r')
-    while True :
-        temp = file.readline().strip().split(',')
-        if temp[0] == "" : break
-        userlist[temp[0]] = temp[1]
-    file.close()
+#新增一個新使用者
+def Signup(user_id,name):
+    url = "https://script.google.com/macros/s/AKfycbxn7Slc2_sKHTc6uEy3zmm3Bh_4duiGCXLavUM3RB0a3yzjAxc/exec"
+    payload = {
+        'sheetUrl':"https://docs.google.com/spreadsheets/d/1qlNktVSkCDH5c6Pj0UC4B_B0q0d6kOT_rMwEgu35VHY/edit?usp=sharing",
+        'sheetTag':"工作表1",
+        'data':user_id+','+name+',-1'
+    }
+    requests.get(url, params=payload)
+
+#取得所有會員資料
+def GetUserList():
+    url = "https://script.google.com/macros/s/AKfycbwVs2Si91yKz6m3utpaPtsttbh_lUQ8LOQM3Zud2hPFxXCgW3u1/exec"
+    payload = {
+        'sheetUrl':"https://docs.google.com/spreadsheets/d/1qlNktVSkCDH5c6Pj0UC4B_B0q0d6kOT_rMwEgu35VHY/edit?usp=sharing",
+        'sheetTag':"工作表1",
+        'row': 1,
+        'col': 1,
+        'endRow' : 51,
+        'endCol' : 20
+    }
+    resp = requests.get(url, params=payload)
+    temp = resp.text.split(',')
+    userlist = []
+    i = 0
+    while i < len(temp):
+        if temp[i] != "":
+            userlist.append(user(temp[i],temp[i+1],temp[i+2]))
+            i+=3
+        else:
+            break
     return userlist
 
-#登入系統
-def Login(event,userlist):
-    i = 0
-    for user in userlist.keys():
-        if event.source.user_id == user:
-            return i
-        i+=1
+#取得目前使用者的index
+def Login(user_id,userlist):
+    for user in userlist:
+        if user.ID == user_id:
+            return userlist.index(user)
     return -1
 
 #寫入資料
-def Update(userlist):
-    file = open('users','w')
-    for user in userlist.keys():
-        file.write(user+','+userlist[user])
-    file.close()
+def Write(Row,data,Col):
+    url = "https://script.google.com/macros/s/AKfycbyBbQ1lsq4GSoKE0yiU5d6x0z2EseeBNZVTewWlSZhQ6EVrizo/exec"
+    payload = {
+        'sheetUrl':"https://docs.google.com/spreadsheets/d/1qlNktVSkCDH5c6Pj0UC4B_B0q0d6kOT_rMwEgu35VHY/edit?usp=sharing",
+        'sheetTag':"工作表1",
+        'data':data,
+        'x':str(Row+1),
+        'y':str(Col)
+    }
+    requests.get(url, params=payload)
 
 #關鍵字系統
-def KeyWord(event):
-    KeyWordDict = {"你好":"你也好啊",
-                   "你是誰":"我是大帥哥",
-                   "帥":"帥炸了",
-                   "差不多了":"讚!!!"}
+def Keyword(event):
+    KeyWordDict = {"你好":["text","你也好啊"],
+                   "你是誰":["text","我是大帥哥"],
+                   "差不多了":["text","讚!!!"],
+                   "帥":["sticker",'1','120']}
 
     for k in KeyWordDict.keys():
         if event.message.text.find(k) != -1:
-            return [True,KeyWordDict[k]]
-    return [False]
-
-#按鈕版面系統
-def Button(event):
-    return TemplateSendMessage(
-        alt_text='特殊訊息，請進入手機查看',
-        template=ButtonsTemplate(
-            thumbnail_image_url='https://github.com/54bp6cl6/LineBotClass/blob/master/logo.jpg?raw=true',
-            title='HPClub - Line Bot 教學',
-            text='大家學會了ㄇ',
-            actions=[
-                PostbackTemplateAction(
-                    label='還沒',
-                    data='還沒'
-                ),
-                MessageTemplateAction(
-                    label='差不多了',
-                    text='差不多了'
-                ),
-                URITemplateAction(
-                    label='幫我們按個讚',
-                    uri='https://www.facebook.com/ShuHPclub'
-                )
-            ]
-        )
-    )
+            if KeyWordDict[k][0] == "text":
+                line_bot_api.reply_message(event.reply_token,TextSendMessage(text = KeyWordDict[k][1]))
+            elif KeyWordDict[k][0] == "sticker":
+                line_bot_api.reply_message(event.reply_token,StickerSendMessage(
+                    package_id=KeyWordDict[k][1],
+                    sticker_id=KeyWordDict[k][2]))
+            return True
+    return False
 
 #指令系統，若觸發指令會回傳True
 def Command(event):
@@ -111,44 +122,51 @@ def Command(event):
     else:
         return False
 
-#新增一個參數
-def Reply(event,userlist):
-    if not Command(event):
-        Ktemp = KeyWord(event)
-        if Ktemp[0]:
-            line_bot_api.reply_message(event.reply_token,
-                TextSendMessage(text = Ktemp[1]))
-        else:
-            if userlist[event.source.user_id] == '-1':
+#回覆函式，指令 > 關鍵字 > 按鈕
+def Reply(event,userlist,clientindex):
+    if userlist[clientindex].Situation == '-1':
+        if not Command(event):
+            if not Keyword(event):
                 line_bot_api.reply_message(event.reply_token,
                     TextSendMessage(text = "你知道台灣最稀有、最浪漫的鳥是哪一種鳥嗎？"))
-                userlist[event.source.user_id] = '0'
-            else:
-                if event.message.text == "黑面琵鷺":
-                    line_bot_api.reply_message(event.reply_token,
-                        TextSendMessage(text = "你居然知道答案!!!"))
-                else:
-                    line_bot_api.reply_message(event.reply_token,
-                        TextSendMessage(text = "答案是：黑面琵鷺!!!因為每年冬天，他們都會到台灣來\"壁咚\""))
-                userlist[event.source.user_id] = '-1'
+                Write(clientindex,'0',3)
+    elif userlist[clientindex].Situation == '0':
+        if event.message.text.find("黑面琵鷺") != -1:
+            line_bot_api.reply_message(event.reply_token,
+                TextSendMessage(text = "你居然知道答案!!!"))
+        else:
+            line_bot_api.reply_message(event.reply_token,
+                TextSendMessage(text = "答案是：黑面琵鷺!!!因為每年冬天，他們都會到台灣來\"壁咚\""))
+        Write(clientindex,'-1',3)
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     try:
-        userlist = GetUserlist()
-        clientindex = Login(event,userlist)
+        userlist = GetUserList()
+        clientindex = Login(event.source.user_id,userlist)
         if clientindex > -1:
-            Reply(event,userlist)
-            '''
-            line_bot_api.push_message("U95418ebc4fffefdd89088d6f9dabd75b", TextSendMessage(text=event.source.user_id + "說:"))
-            line_bot_api.push_message("U95418ebc4fffefdd89088d6f9dabd75b", TextSendMessage(text=event.message.text))
-            '''
+            #開始使用功能
+            line_bot_api.push_message(event.source.user_id, TextSendMessage(text=userlist[clientindex].Name))
+            Reply(event,userlist,clientindex)
         else:
-            userlist[event.source.user_id] = '-1';
-            line_bot_api.reply_message(event.reply_token, 
-                TextSendMessage(text="註冊成功"))
-        Update(userlist)
+            message = TemplateSendMessage(
+                alt_text='確認姓名(手機限定)',
+                template=ConfirmTemplate(
+                    text='初次使用需要登記姓名\n您叫做'+event.message.text+'嗎?',
+                    actions=[
+                        PostbackTemplateAction(
+                            label='對',
+                            data='0`t`'+event.message.text
+                        ),
+                        PostbackTemplateAction(
+                            label='不對',
+                            data='0`f'
+                        )
+                    ]
+                )
+            )
+            line_bot_api.reply_message(event.reply_token, message)
     except Exception as e:
         line_bot_api.reply_message(event.reply_token, 
             TextSendMessage(text=str(e)))
@@ -156,10 +174,26 @@ def handle_message(event):
 #處理Postback
 @handler.add(PostbackEvent)
 def handle_postback(event):
-    command = event.postback.data.split(',')
-    if command[0] == "還沒":
-        line_bot_api.reply_message(event.reply_token,
-            TextSendMessage(text="還沒就趕快練習去~~~"))
+    userlist = GetUserList()
+    clientindex = Login(event.source.user_id,userlist)
+    data = event.postback.data.split('`')
+    #註冊用
+    if data[0] == '0' and clientindex < 0:
+        if data[1] == 't':
+            Signup(event.source.user_id,data[2])
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="註冊成功，歡迎來到LineBot世界"))
+        elif data[1] == 'f':
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="請再次輸入您的姓名"))
+
+#處理貼圖事件
+@handler.add(MessageEvent, message=StickerMessage)
+def handle_sticker_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        StickerSendMessage(
+            package_id='1',
+            sticker_id='410')
+    )
 
 import os
 if __name__ == "__main__":
